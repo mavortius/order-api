ac9cacf230f83979468d2dd2ad87777c004490e1
Marcelo Martins <m.a.martins@live.com>
2019-06-03 10:13:15 -0300

    Setting https config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
+import * as fs from 'fs'
+import * as https from 'https'
 import app from './app'
 
 const PORT = process.env.PORT
 
-app.listen(PORT)
+const httpsOptions = {
+  key: fs.readFileSync('./config/key.pem'),
+  cert: fs.readFileSync('./config/cert.pem'),
+}
+
+https.createServer(httpsOptions, app).listen(PORT)
